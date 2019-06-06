a86d961fd3dea575e25dd1fa46c36d48be0275b6
Facchetti <facchetti.MATOS012@gmail.com>
2019-06-06 17:10:28 -0300

    Commit 6Junho Trabalhando

--- visao/template.php
+++ visao/template.php
@@ -1,15 +1,34 @@
 <html>
-    <head>
-        <title>template MVC</title>
-        <base href="<?= URL_BASE ?>"><!--Esta instrução é super importante e não deve ser mudada!-->
-
-        <link rel="stylesheet" href="./publico/css/app.css">
-    </head>
-    <body class="container">
-
-        <main class="container">
-            <?php require $viewFilePath; ?>
-        </main>
-
-    </body>
+	<head>
+		<title>Electronic's ST</title>
+		<meta charset="utf-8">
+		<base href="<?= URL_BASE ?>">
+        <link rel="shortcut icon" type="image/x-png" href="./publico/img/user/user.png">
+		<link rel="stylesheet" href="./publico/css/app.css">
+	</head>
+	<body class="container">
+		<?php
+			/*
+				http://tiagomatos.com/blog/dica-sobre-a-variavel-global-server-do-php
+				https://www.php.net/manual/pt_BR/reserved.variables.server.php
+			*/
+			if (($_SERVER['PHP_SELF']!='/electronicsst/index.php/cliente/adicionar')&&($_SERVER['PHP_SELF']!='/electronicsst/index.php/login/'))
+			{
+				require('cabecalho.php');
+			}else{
+				require('cabecalho_aux.php');
+			}
+		?>
+		<main class="container" id="content">
+			<?php 
+				require $viewFilePath;
+			?>
+		</main>
+		<?php 
+			if (($_SERVER['PHP_SELF']!='/electronicsst/index.php/cliente/adicionar')&&($_SERVER['PHP_SELF']!='/electronicsst/index.php/login/'))
+			{
+				require('rodape.php');
+			}
+		?>
+	</body>
 </html>
