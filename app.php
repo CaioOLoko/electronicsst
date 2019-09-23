539f9cda3ad23b3638b4d9f6ae0ba1b57ad41018
Facchetti <facchetti.MATOS012@gmail.com>
2019-09-23 10:46:54 -0300

    Introdução ao 4º Bimestre
    Session's, restrições, pedidos e manipulação de arquivos

--- app.php
+++ app.php
@@ -1,7 +1,8 @@
 <?php
 
+require_once "biblioteca/acesso.php";
+require_once "biblioteca/alert.php";
+
 define('CONTROLADOR_PADRAO', 'paginas');
 //define('URL_BASE', 'http://localhost/electronicsst/');
  define('URL_BASE', 'http://electronicsstofficial.000webhostapp.com/');
-
-?>
