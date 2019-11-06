620742459b825ee37e247aaaad39b54d8197228f
Facchetti <facchetti.MATOS012@gmail.com>
2019-11-06 17:28:54 -0300

    Introdução ao display responsivo
    https://www.w3schools.com/css/tryit.asp?filename=tryresponsive_col-s ;
    https://www.w3schools.com/css/css_rwd_intro.asp ;
    https://www.google.com/search?q=responsive+design+css&rlz=1C1GCEB_enBR848BR874&oq=responsive+d&aqs=chrome.5.69i57j0l5.9559j1j7&sourceid=chrome&ie=UTF-8

--- visao/template.php
+++ visao/template.php
@@ -2,6 +2,7 @@
 	<head>
 		<title>Electronic's ST</title>
 		<meta charset="utf-8">
+		<meta name="viewport" content="width=device-width, initial-scale=1.0">
 		<base href="<?= URL_BASE ?>">
 		<link rel="shortcut icon" type="image/x-png" href="publico/img/user/user.png">
 		<link rel="stylesheet" href="publico/css/index.css">
